a630967cc4445ad58591cfcafc462a5c8ad5233d
philthebest123 <111160612+phil000123@users.noreply.github.com>
2023-09-03 20:07:53 -0400

    Add files via upload

---
+++ Walkers.py
@@ -0,0 +1,26 @@
+import cv2
+
+body_classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
+cap = cv2.VideoCapture('walking.avi')
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    bodies = body_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in bodies:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow('Body Detection', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
